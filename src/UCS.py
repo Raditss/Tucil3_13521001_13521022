0fe5c1a8f3f088ad05e790a809d5ac0cab579456
Raditss <raditya.naufal.a@gmail.com>
2023-04-11 04:13:33 +0700

    fix bug pop up and add comment

--- src/UCS.py
+++ src/UCS.py
@@ -1,21 +1,34 @@
 import heapq
 
 def ucs(start, goal, graph):
+    # Initialize the frontier priority queue and dictionaries to store path and cost information
     frontier = []
     heapq.heappush(frontier, (0, start))
     came_from = {}
     cost_so_far = {}
     came_from[start] = None
     cost_so_far[start] = 0
+    
+    # Loop until the goal node is found or the frontier is empty
     while frontier:
+        # Pop the node with the lowest cost so far from the priority queue
         current = heapq.heappop(frontier)[1]
+        
+        # Check if the goal node has been reached
         if current == goal:
             break
+        
+        # Loop through the neighbors of the current node
         for next in graph[current]['edges']:
+            # Calculate the new cost to reach the neighbor
             new_cost = cost_so_far[current] + graph[current]['edges'][next]
+            
+            # If the neighbor hasn't been visited or the new cost is lower than the previous cost, update the dictionaries
             if next not in cost_so_far or new_cost < cost_so_far[next]:
                 cost_so_far[next] = new_cost
                 priority = new_cost
                 heapq.heappush(frontier, (priority, next))
                 came_from[next] = current
+    
+    # Return the path and cost information dictionaries
     return came_from, cost_so_far
--- src/astar.py
+++ src/astar.py
@@ -2,21 +2,37 @@ import heapq
 from extras import calculate_distance
 
 def astar(start, goal, graph):
+    # Initialize the frontier priority queue and dictionaries to store path and cost information
     frontier = []
     heapq.heappush(frontier, (0, start))
     came_from = {}
     cost_so_far = {}
     came_from[start] = None
     cost_so_far[start] = 0
+    
+    # Loop until the goal node is found or the frontier is empty
     while frontier:
+        # Pop the node with the lowest estimated total cost from the priority queue
         current = heapq.heappop(frontier)[1]
+        
+        # Check if the goal node has been reached
         if current == goal:
             break
+        
+        # Loop through the neighbors of the current node
         for next in graph[current]['edges']:
+            # Calculate the new cost to reach the neighbor
             new_cost = cost_so_far[current] + graph[current]['edges'][next]
+            
+            # If the neighbor hasn't been visited or the new cost is lower than the previous cost, update the dictionaries
             if next not in cost_so_far or new_cost < cost_so_far[next]:
                 cost_so_far[next] = new_cost
+                # Calculate the estimated total cost of the path through the neighbor to the goal
                 priority = new_cost + calculate_distance(graph[next]['lat'],graph[goal]['lat'],graph[next]['lon'],graph[goal]['lon'])
+                # Add the neighbor to the frontier with its estimated total cost
                 heapq.heappush(frontier, (priority, next))
+                # Store the parent node of the neighbor in the search tree
                 came_from[next] = current
+    
+    # Return the path and cost information dictionaries
     return came_from, cost_so_far
